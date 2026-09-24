7d500d2d699529d96229bcdc620d9406dd0f62e8
vinayvp <vinaysurfing@gmail.com>
2026-09-24 23:34:01 +0530

    refactor: decouple portfolio into dedicated repository and streamline personal-os routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Apps from "./pages/Apps";
 import NotFound from "./pages/NotFound";
 import AuthGuard from "@/components/AuthGuard";
@@ -18,12 +17,23 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/app" element={
-            <AuthGuard>
-              <Apps />
-            </AuthGuard>
-          } />
+          <Route
+            path="/"
+            element={
+              <AuthGuard>
+                <Apps />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/app"
+            element={
+              <AuthGuard>
+                <Apps />
+              </AuthGuard>
+            }
+          />
+          <Route path="/guest" element={<Apps isGuest={true} />} />
           <Route path="/app/guest" element={<Apps isGuest={true} />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
